fa7919faf2f75a4a7073f298b29492bec53d392b
Ali Farrokhtala <farrokhtala@gmail.com>
2023-08-19 14:09:39 -0700

    first django project

---
+++ my_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World")
+
+
+def about(request):
+    return HttpResponse("About me")
+
+
+def hello(request, first_name):
+    return HttpResponse(f"Hello {first_name}")
+
+
+def add(request, num1, num2):
+    return HttpResponse(f"The total is: {num1 + num2}")
